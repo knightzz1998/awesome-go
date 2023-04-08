c6f49d8d465a2f2a4e2a41f60ff9ef112402c33b
王天赐 <15565946702@163.com>
2023-04-08 18:29:53 +0800

    王天赐, feat: 初始化提交

--- base01/type01.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-
-	// main包是程序的入口, 只有package == main 的才能被编译
-
-}
---
+++ go_base_pkg/type_demo/type_demo.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// main包是程序的入口, 只有package == main 的才能被编译
+	// Base
+	var a bool = true
+	fmt.Print(a)
+
+	var b int = 10
+	fmt.Print(b)
+
+}
